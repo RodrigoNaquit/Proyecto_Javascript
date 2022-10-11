592bd6b17d417073518d5ff134ec6b302f63a147
Rodrigo Naquit <naquitrodrigo@gmail.com>
2022-10-11 20:47:11 -0300

    Preentrega 2, a mejorar

--- JS/main.js
+++ JS/main.js
@@ -4,30 +4,46 @@
 alert ("Selecciona una marca")
 let marca = +(prompt("1-Mercedes 2-Porsche 3-Audi"));
 
+//Constantes
+const Merc1 = {type:"Mercedes Benz", model:"C63", color:"Blanco"};
+const Merc2 = {type:"Mercedes Benz", model:"AMG One", color:"Blanco"};
+const Merc3 = {type:"Mercedes Benz", model:"Maybach", color:"Negro/Gris"};
+
+const Porsche1 = {type:"Porsche", model:"911", color:"Negro"};
+const Porsche2 = {type:"Porsche", model:"GT3", color:"Verde"};
+const Porsche3 = {type:"Porsche", model:"Panamera", color:"Negro"};
+
+const Audi1 = {type:"Audi", model:"S5", color:"Blanco"};
+const Audi2 = {type:"Audi", model:"R8", color:"Nardo Gris"};
+const Audi3 = {type:"Audi", model:"Q7", color:"Negro"};
+
+const autos = [Merc1, Merc2, Merc3, Porsche1, Porsche2, Porsche3, Audi1, Audi2, Audi3];
+
+
 
 // Modelo y Precio
 
 let precioFinal = 0;
 
 if (marca==1){
-    alert ("Elegiste Mercedes")
+    alert ("Elegiste Mercedes Benz")
 
     let modelo = +(prompt("1-C63 2-AMG 3-Maybach"));
 
 switch (modelo) {
     case 1: {
-        alert ("Mercedes C63");
-        precioFinal = precioFinal + 160,000;
+        alert (Merc1.model);
+        precioFinal = precioFinal + 160000;
         break;
     }
     case 2: {
-        alert("Mercedes AMG");
-        precioFinal = precioFinal + 630,000;
+        alert(Merc2.model);
+        precioFinal = precioFinal + 630000;
         break;
     }
     case 3: {
-        alert("Mercedes Mayback");
-        precioFinal = precioFinal + 450,000;
+        alert(Merc3.model);
+        precioFinal = precioFinal + 450000;
         break;
     }
     default:
@@ -42,18 +58,18 @@ switch (modelo) {
 
 switch (modelo) {
     case 1: {
-        alert ("Porsche 911");
-        precioFinal = precioFinal + 330,000;
+        alert (Porsche1.model);
+        precioFinal = precioFinal + 330000;
         break;
     }
     case 2: {
-        alert("Porsche GT3");
-        precioFinal = precioFinal + 430,000;
+        alert(Porsche2.model);
+        precioFinal = precioFinal + 430000;
         break;
     }
     case 3: {
-        alert("Porsche Panamera");
-        precioFinal = precioFinal + 390,000;
+        alert(Porsche3.model);
+        precioFinal = precioFinal + 390000;
         break;
     }
     default:
@@ -68,18 +84,18 @@ switch (modelo) {
 
     switch (modelo) {
         case 1: {
-            alert ("Audi S5");
-            precioFinal = precioFinal + 130,000;
+            alert (Audi1.model);
+            precioFinal = precioFinal + 130000;
             break;
         }
         case 2: {
-            alert("Audi R8");
-            precioFinal = precioFinal + 460,000;
+            alert(Audi2.model);
+            precioFinal = precioFinal + 460000;
             break;
         }
         case 3: {
-            alert("Audi Q7");
-            precioFinal = precioFinal + 150,000;
+            alert(Audi3.model);
+            precioFinal = precioFinal + 150000;
             break;
         }
         default:
@@ -87,9 +103,15 @@ switch (modelo) {
     }
 }
 
-//Precio Final
+//Precio Final, Funcion
+let Tax= precioFinal*0.30
+function precio (precioFinal, Tax) {
+    return precioFinal + Tax;
+}
 
-return precioFinal; 
+//Console
+console.log (Merc2);
+console.log (precioFinal + Tax);
 
 
 
