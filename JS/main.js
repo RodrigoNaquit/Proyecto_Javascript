f3dd806084ea7962da22b560cf2e059cff795579
Rodrigo Naquit <naquitrodrigo@gmail.com>
2022-11-11 21:37:06 -0300

    entrega final

--- JS/main.js
+++ JS/main.js
@@ -1,63 +1,41 @@
-//Simulador de E-Commerce
 
-/*eleccion del Auto
-alert ("Selecciona una marca")
-let marca = +(prompt("1-Mercedes 2-Porsche 3-Audi"));
-*/
-
-var cartId = "cart";
-
-//Constantes
-const Merc1 = {type:"Mercedes Benz", model:"C63", color:"Blanco"};
-const Merc2 = {type:"Mercedes Benz", model:"AMG One", color:"Blanco"};
-const Merc3 = {type:"Mercedes Benz", model:"Maybach", color:"Negro/Gris"};
-
-const Porsche1 = {type:"Porsche", model:"911", color:"Negro"};
-const Porsche2 = {type:"Porsche", model:"GT3", color:"Verde"};
-const Porsche3 = {type:"Porsche", model:"Panamera", color:"Negro"};
-
-const Audi1 = {type:"Audi", model:"S5", color:"Blanco"};
-const Audi2 = {type:"Audi", model:"R8", color:"Nardo Gris"};
-const Audi3 = {type:"Audi", model:"Q7", color:"Negro"};
-
-const marca = document.querySelector("#marca"),
-    modelo = document.querySelector("#modelo"),
-    color = document.querySelector("#color"),
-    precio = document.querySelector("#precio"),
-    img = document.querySelector("#img"),
-    btnGuardar = document.querySelector("#btnGuardar");
 
 //Inventario
-const inventario = [
-    {
+const productos = [
+    {   
+        id:0,
         marca:"Mercedes Benz",
         modelo:"C63 ",
         color: "Blanco",
-        precio: "$160,000 ",
+        precio: 160000,
         img: "./Media/C63.jpeg ",
         },
     {
+        id:1,
         marca:"Mercedes Benz ",
         modelo:"AMG ONE",
         color: "Blanco",
-        precio: "$630,000",
+        precio: 630000,
         img: "./Media/AMG One.webp",
         },
     {   
+        id:2,
         marca:"Mercedes Benz",
         modelo:"Maybach",
         color: "Gris/Negro",
-        precio: "$450,000",
+        precio: 450000,
         img: "./Media/Maybach.png",
         },
 {
+        id:3,
         marca:"Porsche",
         modelo:"911",
         color: "Negro",
         precio: "$330,000",
         img: "./Media/911.jpeg",
         },
-    {
+    {   
+        id:4,
         marca:"Porsche",
         modelo:"GT3",
         color: "Verde",
@@ -65,6 +43,7 @@ const inventario = [
         img: "./Media/GT3.jpg",
         },
     {   
+        id:5,
         marca:"Porsche ",
         modelo:"Panamera",
         color: "Negro",
@@ -72,14 +51,16 @@ const inventario = [
         img: "./Media/Panamera.webp",
         },
 
-{
+{       
+    id:6,
         marca:"Audi",
         modelo:"S5",
         color: "Blanco",
         precio: "$130,000",
         img: "./Media/S5.webp",
         },
-    {
+    {   
+        id:7,
         marca:"Audi",
         modelo:"R8",
         color: "Nardo Gris",
@@ -87,6 +68,7 @@ const inventario = [
         img: "./Media/R8.jpg",
         },
     {   
+        id:8,
         marca:"Audi",
         modelo:"Q7",
         color: "Gris",
@@ -97,153 +79,99 @@ const inventario = [
         
 ]
 
+const productosEl = document.querySelector (".conteiner");
+const cartItemsEl = document.querySelector(".cart")
 
-//Funcion Principal
-function Item (marca, modelo, color, precio , img, cant) {
-    this.marca=marca;
-    this.modelo=modelo;
-    this.color=color;
-    this.precio=parseFloat(precio);
-    if (precio==""){
-        this.precio = 1;
-    } else {
-        this.precio = precio;
-    }
-    this.img=img;
-    this.cant=cant;
+function renderProductos(){
+    productos.forEach( (producto) => {
+        productosEl.innerHTML +=`
+            <h2 class="brand">${producto.marca}</h2>
+                <div class="Mercedes-Benz">
+                    <div>
+                        <img src="${producto.img}" alt=${producto.marca} height="300px" width="450px">
+                        <h3 class="Item"data-name="${producto.modelo}" data-price="${producto.precio}">${producto.modelo}  </h3>  
+                        <button class="add-to-cart-btn" id="btn" value="${producto.id}" onclick="addToCart(${producto.id})" >agregar-al-carrito</button>              
+                    </div>
+        `
+
+    }) 
 }
+renderProductos ();
+//Carrito Array
+let cart = [];
 
-var cart = {
-
-    count: 0,
-    total: 0,
-    items: [],
-    getItems: function () {
-
-        return this.items;
-
-    },
-    setItems: function (items) {
-
-        this.items = items;
-        for (var i = 0; i < this.items.length; i++) {
-            var _item = this.items[i];
-            this.total += _item.total;
-        }
-    },
-    vaciarItems: function () {
-
-        this.items = [];
-        this.total = 0;
-        storage.clearCart();
-        helpers.emptyView();
-
-    },
-    agregarItem: function (item) {
-
-        if (this.containsItem(item.id) === false) {
-
-            this.items.push({
-                id: item.id,
-                name: item.name,
-                price: item.price,
-                count: item.count,
-                total: item.price * item.count
-            });
-
-            storage.saveCart(this.items);
-
-
+//AGREGAR AL CARRITO
+function addToCart(id){
+        if(cart.some((producto) => producto.id==id)){
+            alert("Producto ya se encuentra en el carrito");
         } else {
+    const item = productos.find((producto) => producto.id == id);
 
-            this.updateItem(item);
+    console.log(item, id, productos)
 
-        }
-        this.total += item.price * item.count;
-        this.count += item.count;
-        helpers.updateView();
-
-    },
+    cart.push(item);
+    }
+actualizarCart ();
 }
 
-//Referencias 
-var helpers = {
 
-    getHtml: function (id) {
-
-        return document.getElementById(id).innerHTML;
-
-    },
-    setHtml: function (id, html) {
-
-        document.getElementById(id).innerHTML = html;
-        return true;
-
-    },
-
-    itemData: function (object) {
-        var item = {
-            name: object.getAttribute('data-name'),
-            price: object.getAttribute('data-price'),
-            count: count.value,
-            total: parseInt(object.getAttribute('data-price')) * parseInt(count.value)
-        };
-        return item;
-
-    },
-
-    actualizar: function () {
-        var items = cart.getItems(),
-            template = this.getHtml('cartT'),
-            compiled = _.template(template, {
-                items: items
-            });
-        this.setHtml('cartItems', compiled);
-        this.updateTotal();
-    },
-
-
-    Total: function () {
-        this.setHtml('totalPrice', cart.total + '$');
-    }
-};
-
-
-
-
-//Funcion de Carrito
-var shoppingCart = function() {
-    cart = [];
+//actualizar carrito
+function actualizarCart(){
+    renderItem();
+    precioTotal();
 }
-// Guardar el Carrito
-var localAdapter = {
 
-    saveCart: function (object) {
-        var stringified = JSON.stringify(object);
-        localStorage.setItem(cartId, stringified);
-        return true;
-    },
+//Calcular precio total
+function precioTotal () {
+ let subtotal = producto.precio;
 
-    getCart: function () {
-        return JSON.parse(localStorage.getItem(cartId));
-    },
+}
+//renderizar items en cart
 
-    clearCart: function () {
-        localStorage.removeItem(cartId);
-    }
+function renderItem (){
+    cart.forEach ((producto) => {
+        cartItemsEl.innerHTML += `
+            <div class="cart">
+                <h1>Cart items</h1>
+                <div class="row">
+                    <div class="medium-6 columns">
+                        <img src="${producto.img}" alt="${producto.modelo} width="100px">
+                        <h4> ${producto.marca} ${producto.modelo}</h4>
+                    </div>
+                    <div class="medium-6 columns">
+                         <button class="tiny secondary" id="clear">Clear the cart</button>
+                    </div>
+                    <div class="medium-6 columns">
+                        <button class="tiny disabled" title="Work in progress">Checkout</button>
+                    </div>
+                </div>
+                <div class="total-items"> 0 </div>
+                    Total price: <strong id="totalPrice">${productos.precio}</strong>
+                </div>
+        `
+    });
+}
 
-};
-//Eventos 
-//Sumar al carrito
-document.addEventListener('DOMContentLoaded', function () {
+registrarEventListeners();
 
-    var products = document.querySelectorAll('.Item');
-    [].forEach.call(Item, function (agregarItem) {
-        product.addEventListener('click', function (e) {
+function registrarEventListeners() {
+    cartItemsEl.addEventListener("click",addToCart);
 
-            var item = helpers.itemData(this.parentNode);
-            cart.agregarItem(item);
-        });
+    vaciarElCarrito.addEventListener("click", () => {
+        productosCart = []; 
+        limpiarHTML();
+    })
 
-        });
-})
+    document.addEventListener("DOMContentLoaded", () => {
+        productosCarrito = JSON.parse( localStorage.getItem ("cart")) || [];
+        carritoHTML()
+    });
+}
+
+//Storage
+aplicarStorage()
+        
+
+function aplicarStorage() {
+    localStorage.setItem("cart", JSON.stringify(productosCarrito));
+}
